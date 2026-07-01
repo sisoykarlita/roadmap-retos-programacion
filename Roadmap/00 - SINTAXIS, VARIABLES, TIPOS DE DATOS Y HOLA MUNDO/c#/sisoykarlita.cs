f728cafbf79eea9f107dd11e312fcb1611c8b42e
sisoykarlita <karla.loya@kan-cana.com>
2026-06-30 21:47:57 -0600

    #00-C#

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/sisoykarlita.cs
@@ -0,0 +1,23 @@
+// URL: https://dotnet.microsoft.com/en-us/languages/csharp
+
+// Este es un comentario de una sola línea. También puede usarse al final de una línea de código.
+
+/* Este es un comentario 
+de múltiples líneas.*/
+
+/// Este es un comentario de documentación XML. Se usa para la documentación dde código
+
+// Variables
+int variable = 1; // Para números sin decimales
+double variable2 = 1.0; // Para números con decimales, positivos o negativos
+char variable3 = 'a'; // Para un solo carácter, se encuentra entre comillas simples
+string variable4 = "Hola"; // Para una cadena de caracteres, se encuentra entre comillas dobles
+bool variable5 = true; // Para valores booleanos, puede ser true o false
+
+// Constantes
+const int renta = 1000;
+const double iva = 0.16;
+const char tipo = 'A';
+const string País = "México";
+
+Console.WriteLine("Hola, c#");
